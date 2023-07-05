e2e2ddb157f134ab74bc9be42984a5d99d830e11
Công Lê <levancong_t64@gmail.com>
2023-07-06 01:58:15 +0700

    set Api

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import { ContextApi } from "./context/ContextApi.jsx";
 
 function App() {
   return (
-    <div className="uppercase">
-      hello
-    </div>
+    <ContextApi>
+      <div>hello</div>
+    </ContextApi>
   );
 }
 
